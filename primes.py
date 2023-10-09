4dd19ab431b7ca4755fa5f4ccf21a4b5e496304c
Taha-I <tahaislam1111@gmail.com>
2023-10-09 23:12:32 +0100

    Attempt

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError("Parameter cannot be below 1")
+    else:
+        num = 2
+        while len(list) < number_of_primes:
+            is_prime = True
+            for i in range(2,int((num/2)+1)):
+                if (num % i) == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                list.append(num)
+            num += 1
+                
     return list
